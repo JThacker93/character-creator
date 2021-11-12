c40cc127c4aac280fdf6a62ab18a26bb227a3744
oldsociety <headmistrust@outlook.com>
2021-11-12 13:25:13 -0800

    debugger

---
+++ data/cosmos.js
@@ -0,0 +1,58 @@
+const changeMoonSelect = require("../scripts/index");
+
+const cosmos = [
+  { name: "pandem", type: "planet", color: "red", description: "" },
+  { name: "delis", type: "sun", color: "yellow", description: "" },
+  { name: "veris", type: "sun", color: "red", description: "" },
+  {
+    name: "larimar",
+    type: "moon",
+    color: "green",
+    description: "The small green moon is barely a dot in the night sky.",
+  },
+  {
+    name: "udreth-sol",
+    type: "moon",
+    color: "grey",
+    description:
+      "The largest of the moons, Udreth-Sol is only visible during the day, standing out like a dark grey scar across an otherwise blue sky.",
+  },
+  {
+    name: "pan-shi",
+    type: "moon",
+    color: 'white',
+    description:
+      "Half the size of Udreth-Sol, yet still larger than the rest, Zyry has a warm yellow light which halos the moon at night.",
+  },
+  {
+    name: "nassenti",
+    type: "moon",
+    color: "blue",
+    description:
+      "Pan-shi is easily mistaken to its sister moon, Sen-Shi, as they share equal color and size. The two white moons have opposing orbits and never appear together.",
+  },
+  {
+    name: "zyry",
+    type: "moon",
+    color: "gold",
+    description: "Blue moon with 3 visible rings forming a halo around her.",
+  },
+  {
+    name: "sen-shi",
+    type: "moon",
+    color: "white",
+    description:
+      "Identical to Pan-Shi, when looked extremely closely, the patterns on the face of Sen-shi are upside down to Pan-Shi.",
+  },
+  {
+    name: "oth-orleth",
+    type: "moon",
+    color: "red",
+    description:
+      "This small red moon is easily identifiable in the night sky, except when the red sun is out.",
+  },
+];
+
+
+
+module.exports = cosmos;
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,12 @@
+const cosmos = require("../data/cosmos");
+
 function changeMoonSelect() {
-  if (document.getElementById("dropBox").value == "1") {
-    document.getElementById("divText").innerHTML = document.getElementById(hi).innerHTML;
-  } else if (document.getElementById("dropBox").value == "2") {
-    document.getElementById("divText").innerHTML =
-      cosmos[4].description;
-  } else if (document.getElementById("dropBox").value == "3") {
-    document.getElementById("divText").innerHTML =
-    cosmos[5].description;
-  } else if (document.getElementById("dropBox").value == "4") {
-    document.getElementById("divText").innerHTML =
-    cosmos[6].description;
-  } else if (document.getElementById("dropBox").value == "5") {
-    document.getElementById("divText").innerHTML =
-    cosmos[7].description;
-  } else if (document.getElementById("dropBox").value == "6") {
-    document.getElementById("divText").innerHTML =
-      cosmos[8].description;
-  } else if (document.getElementById("dropBox").value == "7") {
-    document.getElementById("divText").innerHTML =
-      "This small red moon is easily identifiable in the night sky, except when the red sun is out.";
+  if (document.getElementById('dropBox').value == '1') {
+    document.getElementById('divText').innerHTML = cosmos[3].description
   }
+  
 }
 
-
 console.log(cosmos[3].description)
-module.exports = changeMoonSelect;
+
+module.exports = changeMoonSelect()
