db41d97a8f83405faea5d83dd59c98efbad15541
oldsociety <headmistrust@outlook.com>
2021-11-12 12:59:16 -0800

    updated cosmology test

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,27 @@
 function changeMoonSelect() {
   if (document.getElementById("dropBox").value == "1") {
-    document.getElementById("divText").innerHTML =
-      "The small green moon is barely a dot in the night sky.";
+    document.getElementById("divText").innerHTML = document.getElementById(hi).innerHTML;
   } else if (document.getElementById("dropBox").value == "2") {
     document.getElementById("divText").innerHTML =
-      "The largest of the moons, Udreth-Sol is only visible during the day, standing out like a dark grey scar across an otherwise blue sky.";
+      cosmos[4].description;
   } else if (document.getElementById("dropBox").value == "3") {
     document.getElementById("divText").innerHTML =
-      "Half the size of Udreth-Sol, yet still larger than the rest, Zyry has a warm yellow light which halos the moon at night.";
+    cosmos[5].description;
   } else if (document.getElementById("dropBox").value == "4") {
     document.getElementById("divText").innerHTML =
-      "Pan-shi is easily mistaken to its sister moon, Sen-Shi, as they share equal color and size. The two white moons have opposing orbits and never appear together.";
+    cosmos[6].description;
   } else if (document.getElementById("dropBox").value == "5") {
     document.getElementById("divText").innerHTML =
-      "Blue moon with 3 visible rings forming a halo around her.";
+    cosmos[7].description;
   } else if (document.getElementById("dropBox").value == "6") {
     document.getElementById("divText").innerHTML =
-      "Identical to Pan-Shi, when looked extremely closely, the patterns on the face of Sen-shi are upside down to Pan-Shi.";
+      cosmos[8].description;
   } else if (document.getElementById("dropBox").value == "7") {
     document.getElementById("divText").innerHTML =
       "This small red moon is easily identifiable in the night sky, except when the red sun is out.";
   }
 }
 
+
+console.log(cosmos[3].description)
 module.exports = changeMoonSelect;
