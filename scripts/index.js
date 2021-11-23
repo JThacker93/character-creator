de1dea4fddd3d937aee2d0f9dc9e57abc4d46ee9
oldsociety <headmistrust@outlook.com>
2021-11-23 11:19:41 -0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
-const cosmos = require("../data/cosmos");
+const cosmos = require('../data/cosmos')
 
 function changeMoonSelect() {
   if (document.getElementById('dropBox').value == '1') {
     document.getElementById('divText').innerHTML = cosmos[3].description
   }
-  
 }
 
 console.log(cosmos[3].description)
